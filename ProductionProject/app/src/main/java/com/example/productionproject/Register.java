6cbe743cfc3cf154c9e05bda66509ffec381e611
sanjay3241 <smnss3241@gmail.com>
2023-04-18 21:11:50 +0545

    Opening Page

--- ProductionProject/app/src/main/java/com/example/productionproject/Register.java
+++ ProductionProject/app/src/main/java/com/example/productionproject/Register.java
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -19,8 +18,6 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import java.net.PasswordAuthentication;
 
 public class Register extends AppCompatActivity {
 
